1e9353f4a1b6896a30bd7e64a8a1cef152e08da4
Jonatan <jgbattista4@gmail.com>
2025-05-27 19:29:17 -0300

    Initial commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+const CartWidget = () => {
+  return (
+    <a href="#" className="text-white position-relative">
+      <span role="img" aria-label="carrito" style={{ fontSize: "1.5rem" }}>
+        🛒
+      </span>
+    </a>
+  );
+};
+
+export default CartWidget;
