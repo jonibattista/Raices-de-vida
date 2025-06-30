2109e27cc76a9b654fe4ccde7626c83292fc012e
Jonatan <72102622+jonibattista@users.noreply.github.com>
2025-06-29 21:27:05 -0300

    Add files via upload

--- Raices-de-vida/src/App.jsx
+++ Raices-de-vida/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
 
 function App() {
   return (
     <>
       <NavBar />
-      <ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+        <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
+      </Routes>
     </>
   );
 }
